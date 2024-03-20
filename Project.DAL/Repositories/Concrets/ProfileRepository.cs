3bd70f654086a1ec712f573a404d224901b9b386
enisruyaa@gmail.com <enisruyaa@gmail.com>
2024-03-20 22:12:51 +0300

    Dal Katmanın'da Repositoryies klasöründe Abstract ve Concrete Repository'lerimizi oluşturduk

---
+++ Project.DAL/Repositories/Concrets/ProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using Project.DAL.ContextClasses;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concrets
+{
+    public class ProfileRepository : BaseRepository<AppUserProfile>
+    {
+        public ProfileRepository(MyContext db) : base(db) 
+        {
+
+        }
+    }
+}
