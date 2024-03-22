b7629368f39df470dd6fb05c760d3970d3219c4e
enisruyaa@gmail.com <enisruyaa@gmail.com>
2024-03-22 22:37:11 +0300

    Bll Katmanında Manager'ın IOC tanımlamaları yapıldı

---
+++ Project.BLL/ServiceInjections/ManagerService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.Manager.Abstracts;
+using Project.BLL.Manager.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.ServiceInjections
+{
+    public static class ManagerService
+    {
+        public static IServiceCollection AddManagerServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IManager<>), typeof(BaseManager<>));
+
+            services.AddScoped<IAppUserManager, AppUserManager>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<IOrderManager, OrderManager>();
+            services.AddScoped<IOrderDetailManager, OrderDetailManager>();
+            services.AddScoped<IProductManager, ProductManager>();
+            services.AddScoped<IProfileManager, ProfileManager>();
+            return services;
+        }
+    }
+}
