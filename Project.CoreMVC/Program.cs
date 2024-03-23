b5e32daa0abca6ed1aa0674165005f8cd86e1178
enisruyaa@gmail.com <enisruyaa@gmail.com>
2024-03-23 19:12:06 +0300

    ManagerService ve RepService IOC matematigi middleware'e BLL alınarak yazılır

--- Project.CoreMVC/Program.cs
+++ Project.CoreMVC/Program.cs
@@ -5,10 +5,22 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache(); // Eðer Session Kompleks yapýlar içiin Extension meyodu ekleme durumuna maruz kalýrsa bu kod projemizin saðlýklý çalýþmasý için gereklidir.
+
+builder.Services.AddSession(x =>
+{
+    x.IdleTimeout = TimeSpan.FromDays(1); //Projeyi kiþinin bos durma süresi eger 5 dakikalýk bir süre olursa Session bosa cýksýn
+    x.Cookie.HttpOnly = true; //document.cookie'den ilgili bilginin gözlemlenmesi
+    x.Cookie.IsEssential = true;
+});
+
+builder.Services.AddIdentityServices();
 
 builder.Services.AddDbContextServices(); // DbContextService'imizi BLL'den alarak middleware!'e entegre etttik.
 
-builder.Services.AddIdentityServices();
+builder.Services.AddRepServices();
+
+builder.Services.AddManagerServices();
 
 
 WebApplication app = builder.Build();
@@ -21,6 +33,10 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession(); // Sesiion kullanýlabilir olsun diye yazmamýz gerekiyor.
+
+app.UseAuthentication(); // Rol tanýmlamasý olmdan görev tanýmlamasý olmaz.
 
 app.UseAuthorization();
 
